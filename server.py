71fb4806e02b581b1feca9d95e3ceb3300c29346
Jason Tyler <jmtyler@gmail.com>
2015-06-30 19:26:52 -0700

    saving intermediate progress; server.py is close to full step1 form

--- server.py
+++ server.py
@@ -1,34 +1,37 @@
 import socket
 import sys
 
-ADDR = ('127.0.0.1', 8001) #port 0 may force os to find an open port
+ADDR = ('127.0.0.1', 8001)  # port 0 may force os to find an open port
+
 
 def setup_server():
     server_socket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(ADDR)
-    server_socket.listen(1) #1 is backlog of connections it will hold onto
+    server_socket.listen(1)
     return server_socket
 
 
 def response_ok():
-    """ returns a response containg an inital response line, header, body"""
-    response_ok = """HTTP/1.1 200 OK\r\n\
+    """Return a status 200 HTTP response_ok"""
+    response_ok = b"""HTTP/1.1 200 OK\r\n\
     DATE: Sun, 21 Jul 2001 23:32:15 GTM\r\n\
     SERVER: Python/2.7.6\r\n\
     \r\n\
     """
-    return response_ok.encode('utf-8')
+    return response_ok
+
 
 def response_error():
-    "returns an an HTTP 500 error"
-    response_error = """HTTP 500 Internal Server Error \r\n\
+    """Return a status 500 Internal Server Error"""
+
+    response_error = b"""HTTP 500 Internal Server Error \r\n\
     DATE: Sun, 21 Jul 2001 23:32:15 GTM\r\n\
     SERVER: Python/2.7.6\r\n\
     \r\n\
     """
-    return response_error.encode('utf-8')
+    return response_error
 
 
 def main():
@@ -36,13 +39,12 @@ def main():
 
     while True:
         try:
-            conn, addr =  server_socket.accept()
+            conn, addr = server_socket.accept()
             msg = ""
             while True:
                 msg_chunk = conn.recv(1024)
                 msg += msg_chunk
-                # conn.sendall(msg)
-                if len(msg_chunk) <  1024:
+                if len(msg_chunk) < 1024:
                     # response_ok()
                     conn.sendall(msg)
                     conn.close()
@@ -50,12 +52,8 @@ def main():
             sys.stdout.write(msg)
 
         except KeyboardInterrupt:
-            client.close()
+            conn.close()
             break
-    
 
 if __name__ == '__main__':
     main()
-
-
-# #find setting in doc to fix port is in use > dynamically?
