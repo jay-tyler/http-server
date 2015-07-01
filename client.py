f6324221c5358283adb24b634e573244aecc0723
HeyIamJames <jameshemmaplardh@gmail.com>
2015-06-30 18:51:34 -0700

    added base server and client code

--- client.py
+++ client.py
@@ -1,34 +1,56 @@
+'''
+    Simple socket server using threads
+'''
+ 
 import socket
-
-ADDR = ('127.0.0.1', 8011) #port 0 may force os to find an open port
-
-client = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
-)
-
-client.connect(ADDR)
-msg = "client is connected"
-
-def main():
+import sys
+from thread import *
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+ 
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+     
+    #infinite loop so that function do not terminate and thread do not end.
     while True:
-        try: 
-            client.sendall(msg)
-            while True:
-                part = client.recv(16)
-                client.shutdown(socket.SHUT_WR)
-                print part
-                if len(part) < 16:
-                    client.close()
-                    break
-        except Exception as e:
-            client.close()
-            print msg
+         
+        #Receiving from client
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        if not data: 
             break
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-# #find setting in doc to fix port is in use > dynamically?
+     
+        conn.sendall(reply)
+     
+    #came out of loop
+    conn.close()
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+ 
+s.close()
--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import socket
+import sys
 
-ADDR = ('127.0.0.1', 8011) #port 0 may force os to find an open port
+ADDR = ('127.0.0.1', 8001) #port 0 may force os to find an open port
 
-socket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
-)
+def setup_server():
+    server_socket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(ADDR)
+    server_socket.listen(1) #1 is backlog of connections it will hold onto
+    return server_socket
 
-socket.bind(ADDR)
-socket.listen(1) #1 is backlog of connections it will hold onto
 
 def response_ok():
     """ returns a response containg an inital response line, header, body"""
@@ -28,28 +31,41 @@ def response_error():
     return response_error.encode('utf-8')
 
 
+def parse_request(header):
+    "phrase header, check values against method and proto"
+    header = header.split('\r\n')
+    first_line = header[0].split()
+    try:
+        method = first_line[0]
+        uri = first_line[1]
+    if method != 'GET' or proto != 'HTTP/1.1':
+        return response_error(<code>,<reason>)
+
+    except IndexError:
+        pass
+    return uri
+
 def main():
-    msg = ""
+    server_socket = setup_server()
+
     while True:
         try:
-            conn, addr =  socket.accept()
+            conn, addr = server_socket.accept()
+            msg = ""
             while True:
-                msg += conn.recv(16)
-                conn.sendall(msg)
-                if len(msg) <  16:
+                msg_chunk = conn.recv(1024)
+                msg += msg_chunk
+                # conn.sendall(msg)
+                if len(msg_chunk) < 1024:
                     # response_ok()
+                    conn.sendall(msg)
                     conn.close()
                     break
+            sys.stdout.write(msg)
+
         except KeyboardInterrupt:
             client.close()
             break
-    # sys.stdout.write(msg)
-    
-
-
 
 if __name__ == '__main__':
     main()
-
-
-#find setting in doc to fix port is in use > dynamically?
