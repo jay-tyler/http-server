02f63a9e543a66e7ebcba8c1220526f8d76de9d6
HeyIamJames <jameshemmaplardh@gmail.com>
2015-07-02 08:50:53 -0700

    changed server.py

--- server.py
+++ server.py
@@ -1,8 +1,36 @@
+# -*- coding: utf-8 -*-
 import socket
 import sys
 
-ADDR = ('127.0.0.1', 8044)  # port 0 may force os to find an open port
+ADDR = ('127.0.0.1', 8024)
 
+CRLF = b'\r\n'
+GET = b'GET'
+PROTOCOL = b'HTTP/1.1'
+HOST_PREFIX = b'Host:'
+
+
+Response = CRLF.join([
+    b'HTTP/1.1 {response_code} {response_reason}',
+    b'Content-Type: text/html; charset=UTF-8',
+    b''])
+
+def parse_request(request):
+    lines = request.split(CRLF)
+    header = lines[0]
+    header_pieces = header.split()
+    if header_pieces[0] != GET:
+        raise TypeError(b'Method Not Allowed')
+    elif header_pieces[2] != PROTOCOL:
+        raise ValueError(b'HTTP Version Not Supported')
+    host_line = lines[1]
+    host_line_pieces = host_line.split()
+    if lines[2] != b'':
+        raise SyntaxError(b'Bad Request')
+    if host_line_pieces[0] != HOST_PREFIX:
+        raise Exception(b'Bad Request')
+    else:
+        return header_pieces[1]
 
 def setup_server():
     server_socket = socket.socket(
@@ -14,30 +42,32 @@ def setup_server():
 
 
 def response_ok():
-    """Return a status 200 HTTP response_ok"""
+    return Response.format(response_code=b'200',
+                           response_reason=b'OK')
 
-    response_ok = b"""HTTP/1.1 200 OK\r\n
-    DATE: Sun, 21 Jul 2001 23:32:15 GTM\r\n
-    SERVER: Python/2.7.6\r\n
-    \r\n"""
+    # """Return a status 200 HTTP response_ok"""
+    # response_ok = b"""HTTP/1.1 200 OK\r\n
+    # DATE: Sun, 21 Jul 2001 23:32:15 GTM\r\n
+    # SERVER: Python/2.7.6\r\n
+    # \r\n"""
 
-    return response_ok
 
 
 def response_error():
-    """Return a status 500 Internal Server Error"""
+    return Response.format(response_code=b'200',
+                           response_reason=b'response_reason')
+    # """Return a status 500 Internal Server Error"""
 
-    response_error = b"""HTTP 500 Internal Server Error \r\n
-    DATE: Sun, 21 Jul 2001 23:32:15 GTM\r\n
-    SERVER: Python/2.7.6\r\n
-    \r\n"""
+    # response_error = b"""HTTP 500 Internal Server Error \r\n
+    # DATE: Sun, 21 Jul 2001 23:32:15 GTM\r\n
+    # SERVER: Python/2.7.6\r\n
+    # \r\n"""
+    
 
-    return response_error
 
 
 def main():
     server_socket = setup_server()
-
     while True:
         try:
             conn, addr = server_socket.accept()
@@ -46,12 +76,18 @@ def main():
                 msg_chunk = conn.recv(1024)
                 msg += msg_chunk
                 if len(msg_chunk) < 1024:
-                    # response_ok()
                     conn.sendall(response_ok())
                     conn.close()
-                    break
+            try:
+                parse_request(msg)
+            except SyntaxError:
+                response_error(400, "Invalid SyntaxError")
+            except ValueError:
+                response_error(400, "Value Error")
+                #addmore
             sys.stdout.write(msg)
-
+            conn.close()
+            break
         except KeyboardInterrupt:
             conn.close()
             break
