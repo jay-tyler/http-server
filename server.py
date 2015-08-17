db750360a53a8c5716d65b0b91582667f493df06
Jason Tyler <jmtyler@gmail.com>
2015-08-16 17:50:42 -0700

    finished with the exception of having something odd in image encoding

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
 import socket
 import sys
 import os
@@ -7,7 +6,7 @@ import mimetypes
 
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'webroot')
 ROOT = ROOT.encode('utf-8')
-ADDR = (b'127.0.0.1', 8000)
+ADDR = (b'127.0.0.1', 8004)
 CRLF = (b'\r\n')
 PROTOCOL = b'HTTP/1.1'
 # Using this as a dummy var
@@ -19,7 +18,7 @@ reqtypes = set([b"POST", b"GET", b"PUT", b"HEAD", b"DELETE", b"OPTIONS",
 RESPONSE = CRLF.join([
     b'HTTP/1.1 {response_code} {response_reason}',
     b'Content-Type: {ctype}; charset=UTF-8',
-    b'Date: {date}', CRLF, b'{message}'])
+    b'Date: {date}', b'', b'{message}'])
 
 
 def parse_request(request):
@@ -31,7 +30,7 @@ def parse_request(request):
         * Request is HTTP/1.1
         * Request include valid host header
     if these validations are met, then return URI from request"""
-    request = request.encode('utf-8') # TODO: would want to query encoding
+    request = request.decode('utf-8')
     request = request.strip(CRLF).strip()
     lines = request.split(CRLF)
     initial_line = lines[0]
@@ -44,37 +43,28 @@ def parse_request(request):
     headers = [line.split()[0].strip() for line in lines][1:]
     #  Grabbing each header from above, removing trailing colon and converting
     #  to uppercase
-    headers = [header.rstrip(b':').upper() for header in headers]
+    headers = [header.rstrip(':').upper() for header in headers]
     #  Converting headers to set for ease of membership testing
     headers = set(headers)
     if reqmethod not in reqtypes:
         #  HTTP request is invalid; containing fct should return
         #  400 Bad Request
         raise ValueError
-    elif b'HTTP/1.1' not in protocol:
+    elif 'HTTP/1.1' not in protocol:
         #  HTTP request is for a different protocol; containing fct should
         #  return 505 HTTP Version Not Supported
         raise NotImplementedError
-    elif b'GET' not in reqmethod:
+    elif 'GET' not in reqmethod:
         #  HTTP request is for unsupported method; containing fct should
         #  return 405 Method Not Allowed
         raise IndexError
-    elif b'HOST' not in headers:
+    elif 'HOST' not in headers:
         #  HTTP request is not properly formed; containing fct should
         #  return 400 Bad Request
         raise ValueError
     else:
         #  HTTP request passes all prior checks, pass URI back
         return uri
-
-
-def setup_server():
-    server_socket = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(ADDR)
-    server_socket.listen(1)
-    return server_socket
 
 
 def response_ok(uri):
@@ -134,13 +124,22 @@ def resolve_uri(uri):
     elif os.path.exists(pth):
         with open(pth, 'r') as file:
             message = file.read()
+            # message = "dsfasdfasd"
         ctype = mimetypes.guess_type(pth)[0]
-
     else:
         raise LookupError  # For 404
 
-    print pth, ROOT
     return message, ctype
+
+
+def setup_server():
+    server_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(ADDR)
+    server_socket.listen(1)
+    return server_socket
+
 
 def main():
     server_socket = setup_server()
@@ -169,7 +168,6 @@ def main():
                     conn.sendall(response)
                     conn.close()
                     break
-            sys.stdout.write(msg)
 
         except KeyboardInterrupt:
             conn.close()
--- test_server.py
+++ test_server.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 import socket
 import pytest
 import server
@@ -9,7 +8,7 @@ from multiprocessing import Process
 ###################################################
 # Constants
 ###################################################
-ADDR = (b'127.0.0.1', 8000)
+ADDR = (b'127.0.0.1', 8004)
 CRLF = b'\r\n'
 DUMMY_DATE = b"Sun, 21 Jul 2001 23:32:15 GTM"
 
@@ -94,9 +93,7 @@ def parse_response(response):
     protocol = initial_line.split()[0].strip().upper()
     response_code = initial_line.split()[1].strip()
     headers = [line for line in lines if b':' in line]
-    headers = [line.split(':')[0].strip().upper() for line in lines]
-    # headers = [header.rstrip(b':').upper() for header in headers]
-    #  Converting headers to set for ease of membership testing
+    headers = [line.split(b':')[0].strip().upper() for line in lines]
     headers = set(headers)
 
     try:
@@ -144,19 +141,42 @@ def test_parse_bad_host_request():
 def test_response_ok():
     foo_uri = b'http://www.host.com'
     response = server.response_ok(foo_uri)
-    assert parse_response(response)[0] == b'200'
+    assert parse_response(response)[0] == '200'
 
 
 def test_response_error():
     response = server.response_error(500, b"Internal Server Error")
-    assert parse_response(response)[0] == b'500'
+    assert parse_response(response)[0] == '500'
+
+
+def test_resolve_uri_file():
+    body, ctype = server.resolve_uri("http://www.host.com/sample.txt")
+    assert ctype == b'text/plain'
+    assert body is not None
+
+
+def test_resolve_uri_file():
+    body, ctype = server.resolve_uri("http://www.host.com/sample.txt")
+    assert ctype == b'text/plain'
+    assert b'This is a very simple text file.' in body
+
+
+def test_resolve_uri_dir():
+    body, ctype = server.resolve_uri("http://www.host.com/")
+    assert ctype == b'text/html'
+    assert b'a_web_page.html' in body
+    assert b'JPEG_example.jpg' not in body
+
+
+def test_resolve_uri_invalid():
+    with pytest.raises(LookupError):
+        server.resolve_uri("http://www.host.com/blah")
 
 
 ###################################################
 # Functional Tests
 ###################################################
 def test_functional_test_of_bad_request(client_socket):
-
     client_socket.connect(ADDR)
     client_socket.sendall(b"Hello there.")
     while True:
@@ -164,18 +184,17 @@ def test_functional_test_of_bad_request(client_socket):
         if len(response) < 1024:
             break
     # import pdb; pdb.set_trace()
-    assert parse_response(response)[0] == b"405"
+    assert parse_response(response)[0] == "405"
 
 
 def test_functional_test_of_good_request(client_socket):
-
     client_socket.connect(ADDR)
     client_socket.sendall(REQ_GOOD)
     while True:
         response = client_socket.recv(1024)
         if len(response) < 1024:
             break
-    assert parse_response(response)[0] == b"200"
+    assert parse_response(response)[0] == "200"
 
 
 def test_functional_request_of_dir(client_socket):
@@ -194,3 +213,55 @@ def test_functional_request_of_dir(client_socket):
     assert parse_response(response)[0] == b'200'
     assert b'a_web_page.html' in response
     assert b'JPEG_example.jpg' not in response
+
+
+def test_functional_request_of_dir(client_socket):
+    request = Response_SKEL.format(Response=b'get',
+        requri=b'http://www.host.com/',
+        protocol=b"HTTP/1.1", host=b"www.host.com",
+        date=DUMMY_DATE)   
+
+    client_socket.connect(ADDR)
+    client_socket.sendall(request)
+    while True:
+        response = client_socket.recv(1024)
+        if len(response) < 1024:
+            break
+    assert b'text/html' in response
+    assert parse_response(response)[0] == b'200'
+    assert b'a_web_page.html' in response
+    assert b'JPEG_example.jpg' not in response
+
+
+def test_functional_request_of_text_file(client_socket):
+    request = Response_SKEL.format(Response=b'get',
+        requri=b'http://www.host.com/sample.txt',
+        protocol=b"HTTP/1.1", host=b"www.host.com",
+        date=DUMMY_DATE)   
+
+    client_socket.connect(ADDR)
+    client_socket.sendall(request)
+    while True:
+        response = client_socket.recv(1024)
+        if len(response) < 1024:
+            break
+    assert b'text/plain' in response
+    assert parse_response(response)[0] == b'200'
+    assert b'This is a very simple text file.' in response
+
+
+def test_functional_request_of_image_file(client_socket):
+    request = Response_SKEL.format(Response=b'get',
+        requri=b'http://www.host.com/images/JPEG_example.jpg',
+        protocol=b"HTTP/1.1", host=b"www.host.com",
+        date=DUMMY_DATE)
+
+    client_socket.connect(ADDR)
+    client_socket.sendall(request)
+    while True:
+        response = client_socket.recv(1024)
+        if len(response) < 1024:
+            break
+    code, body = parse_response(response)
+    assert code == b'200'
+    assert b'image/jpeg' in response[:-2]
